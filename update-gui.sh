b80d7f1e251ccb93b9173210f662bbb375abec33
Shawn Wilkinson <me@super3.org>
2021-05-05 08:17:25 -0400

    Create update-gui.sh

---
+++ update-gui.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+cd chia-blockchain
+chia stop -d all
+deactivate
+git fetch
+git checkout latest
+git reset --hard FETCH_HEAD
+
+# If you get RELEASE.dev0 then delete the package-lock.json in chia-blockchain-gui and install.sh again
+
+git status
+
+# git status should say "nothing to commit, working tree clean", 
+# if you have uncommitted changes, RELEASE.dev0 will be reported.
+
+sh install.sh
+
+. ./activate
+
+chia init
+
+# The GUI requires you have Ubuntu Desktop or a similar windowing system installed.
+# You can not install and run the GUI as root
+cd chia-blockchain-gui
+git fetch
+cd ..
+chmod +x ./install-gui.sh
+./install-gui.sh
+
+cd chia-blockchain-gui
+npm run electron &
